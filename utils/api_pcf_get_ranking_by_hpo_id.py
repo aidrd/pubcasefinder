9b458edc93c960232bd98396318bfbb9a7fa0428
Toyofumi Fujiwara <fujiwara@dbcls.rois.ac.jp>
2021-06-25 07:35:56 +0900

    update the gene ranking method

--- utils/api_pcf_get_ranking_by_hpo_id.py
+++ utils/api_pcf_get_ranking_by_hpo_id.py
@@ -66,17 +66,17 @@ def pcf_get_ranking_by_hpo_id(r_target, r_phenotype):
     ### http://stackoverflow.com/questions/4574609/executing-select-where-in-using-mysqldb
     sql_index = ""
     if r_target=="omim":
-        sql_index = u"select a.OntoIDOMIM, a.IndexOntoIDHP, a.DiseaseOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexDiseaseHPOMIM as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.IndexOntoIDHP in (%s) and b.OntoName='HP'"
+        sql_index = u"select a.OntoIDOMIM, a.IndexOntoIDHP, a.DiseaseOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexDiseaseHPOMIM as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.IndexOntoIDHP in (%s) and b.OntoName='HP' order by field(a.IndexOntoIDHP, %s)"
     elif r_target=="orphanet":
-        sql_index = u"select a.OntoIDORDO, a.IndexOntoIDHP, a.DiseaseOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexDiseaseHP as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.OntoIDORDO in (select distinct OntoID from Orphanet where RareDiseaseFlg=1) and a.IndexOntoIDHP in (%s) and b.OntoName='HP'"
+        sql_index = u"select a.OntoIDORDO, a.IndexOntoIDHP, a.DiseaseOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexDiseaseHP as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.OntoIDORDO in (select distinct OntoID from Orphanet where RareDiseaseFlg=1) and a.IndexOntoIDHP in (%s) and b.OntoName='HP' order by field(a.IndexOntoIDHP, %s)"
     elif r_target=="gene":
-        sql_index = u"select a.EntrezID, a.IndexOntoIDHP, a.GeneOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexGenePhenotypeFromHPO as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.IndexOntoIDHP in (%s) and b.OntoName='HP'"
+        sql_index = u"select a.NCBIGeneID_MONDOID, a.IndexOntoIDHP, a.GeneOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexGPA as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.IndexOntoIDHP in (%s) and b.OntoName='HP' order by field(a.IndexOntoIDHP, %s)"
     elif r_target=="case":
-        sql_index = u"select a.CaseID, a.IndexOntoIDHP, a.CaseOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexCaseHP as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.IndexOntoIDHP in (%s) and b.OntoName='HP'"
+        sql_index = u"select a.CaseID, a.IndexOntoIDHP, a.CaseOntoIDHP, a.CommonRootHPIC, (b.IC - a.CommonRootHPIC) from IndexCaseHP as a left join IC as b on a.IndexOntoIDHP=b.OntoID where a.IndexOntoIDHP in (%s) and b.OntoName='HP' order by field(a.IndexOntoIDHP, %s)"
     in_p=', '.join(map(lambda x: '%s', list_phenotypes))
-    sql_index = sql_index % in_p
+    sql_index = sql_index % (in_p, in_p)
     cursor_index = OBJ_MYSQL.cursor()
-    cursor_index.execute(sql_index, list_phenotypes)
+    cursor_index.execute(sql_index, (list_phenotypes + list_phenotypes))
     values_index = cursor_index.fetchall()
     cursor_index.close()
 
@@ -130,20 +130,34 @@ def pcf_get_ranking_by_hpo_id(r_target, r_phenotype):
     # 類似疾患検索結果を収納
     for onto_id in dict_similar_diseases.keys():
 
+        geneid = ""
+        if r_target=="gene":
+            list_NCBIGeneID_MONDOID = onto_id.split("_")
+            geneid = (list_NCBIGeneID_MONDOID[0]).replace('GENEID', 'ENT')
+
         # IndexDiseaseHPOMIMの中に、オントロジーには含まれないOMIM IDが含まれているため、それらの処理を飛ばす
         ## TODO: 実際は、IndexDiseaseHPOMIM関連から、データの作成時にそれらのOMIM IDを除く必要がある。 20180913 藤原
-        if onto_id not in dict_AnnotationHPONum:
-            continue
+        if r_target=="gene":
+            if geneid not in dict_AnnotationHPONum:
+                continue
+        else:
+            if onto_id not in dict_AnnotationHPONum:
+                continue
 
         dict_similar_disease = {}
         dict_similar_disease['score']                = float(dict_similar_diseases[onto_id]['sum_ic'] / dict_similar_diseases[onto_id]['sum_ic_denominator']) if dict_similar_diseases[onto_id]['sum_ic_denominator'] != 0 else 0
-        dict_similar_disease['matched_hpo_id']       = ",".join(dict_similar_diseases[onto_id]['matched_hpo_id'])
-        dict_similar_disease['annotation_hp_num']    = dict_AnnotationHPONum[onto_id]
-        dict_similar_disease['annotation_hp_sum_ic'] = dict_AnnotationHPOSumIC[onto_id]
+        list_matched_hpo_id = []
+        for x in dict_similar_diseases[onto_id]['matched_hpo_id']:
+            if x not in list_matched_hpo_id:
+                list_matched_hpo_id.append(x)
+        #dict_similar_disease['matched_hpo_id']       = ",".join(dict_similar_diseases[onto_id]['matched_hpo_id'])
+        dict_similar_disease['matched_hpo_id']       = ",".join(list_matched_hpo_id)
+        dict_similar_disease['annotation_hp_num']    = dict_AnnotationHPONum[onto_id] if not r_target=="gene" else dict_AnnotationHPONum[geneid]
+        dict_similar_disease['annotation_hp_sum_ic'] = dict_AnnotationHPOSumIC[onto_id] if not r_target=="gene" else dict_AnnotationHPOSumIC[geneid]
         if r_target=="orphanet":
             onto_id = onto_id.replace('ORDO', 'ORPHA')
-        elif r_target=="gene":
-            onto_id = onto_id.replace('ENT', 'GENEID')
+        #elif r_target=="gene":
+        #    onto_id = onto_id.replace('ENT', 'GENEID')
         dict_similar_disease['id']                   = onto_id
         if r_target=="case":
             if re.compile("DECIPHER").search(onto_id):
@@ -164,19 +178,40 @@ def pcf_get_ranking_by_hpo_id(r_target, r_phenotype):
     rank             = 0
     rank_deposit     = 0
     prev_match_score = 0
+    dict_rank_NCBIGeneID = {}
     for dict_similar_disease in sorted(list_dict_similar_disease, key=lambda x: (-float(x['score']),int(x['annotation_hp_num']),-float(x['annotation_hp_sum_ic']))):
 
-        if dict_similar_disease['score'] != prev_match_score:
-            rank = rank + 1 + rank_deposit 
-            dict_similar_disease['rank'] = rank
-            prev_match_score = dict_similar_disease['score']
-            rank_deposit = 0
-        else:
-            dict_similar_disease['rank'] = rank
-            prev_match_score = dict_similar_disease['score']
-            rank_deposit += 1
+        if r_target=="gene":
+            list_NCBIGeneID_MONDOID = (dict_similar_disease['id']).split("_")
+            NCBIGeneID = list_NCBIGeneID_MONDOID[0]
+            MONDOID    = list_NCBIGeneID_MONDOID[1]
+            if NCBIGeneID in dict_rank_NCBIGeneID:
+                continue
 
-        list_dict_similar_disease_sorted.append(dict_similar_disease)
+            if dict_similar_disease['score'] != prev_match_score:
+                rank = rank + 1 + rank_deposit 
+                dict_similar_disease['rank'] = rank
+                prev_match_score = dict_similar_disease['score']
+                rank_deposit = 0
+            else:
+                dict_similar_disease['rank'] = rank
+                prev_match_score = dict_similar_disease['score']
+                rank_deposit += 1
+            dict_similar_disease['id'] = NCBIGeneID
+            list_dict_similar_disease_sorted.append(dict_similar_disease)
+            dict_rank_NCBIGeneID[NCBIGeneID] = MONDOID
+        else:
+            if dict_similar_disease['score'] != prev_match_score:
+                rank = rank + 1 + rank_deposit 
+                dict_similar_disease['rank'] = rank
+                prev_match_score = dict_similar_disease['score']
+                rank_deposit = 0
+            else:
+                dict_similar_disease['rank'] = rank
+                prev_match_score = dict_similar_disease['score']
+                rank_deposit += 1
+            list_dict_similar_disease_sorted.append(dict_similar_disease)
+
 
     return list_dict_similar_disease_sorted
 
