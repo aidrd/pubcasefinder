086f2f9eb9e046f093e55259e0c118ca53b9e6ac
スコッチ <43601554+swallow-ds@users.noreply.github.com>
2022-09-10 12:39:06 +0900

    add js

--- record/js/main.js
+++ record/js/main.js
@@ -3,12 +3,12 @@ $(document).on('click', '.open-options', function (event) {
   $('#modal-options').iziModal('open');
 });
 $('#modal-options').iziModal({
-  headerColor: '#26A69A', //ヘッダー部分の色
-  width: 400, //横幅
-  overlayColor: 'rgba(0, 0, 0, 0.5)', //モーダルの背景色
-  fullscreen: true, //全画面表示
-  transitionIn: 'fadeInUp', //表示される時のアニメーション
-  transitionOut: 'fadeOutDown' //非表示になる時のアニメーション
+  headerColor: '#26A69A',
+  width: 400,
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+  fullscreen: true,
+  transitionIn: 'fadeInUp',
+  transitionOut: 'fadeOutDown'
 });
 
 
