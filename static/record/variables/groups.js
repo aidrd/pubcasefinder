1811e2be58185cfadcd6beab158c94456a994770
スコッチ <43601554+swallow-ds@users.noreply.github.com>
2022-09-23 15:46:58 +0900

    add css

---
+++ static/record/variables/groups.js
@@ -0,0 +1,22 @@
+let groups = [
+    {
+        "id": "G00000000001",
+        "name": "Honda",
+        "description": "ホンダです"
+    },
+    {
+        "id": "G00000000002",
+        "name": "Toyota",
+        "description": "トヨタです"
+    },
+    {
+        "id": "G00000000003",
+        "name": "Hyundai-01",
+        "description": "hyundaiです"
+    },
+    {
+        "id": "G00000000004",
+        "name": "Tesla",
+        "description": "teslaです"
+    }
+]
