d39d3cb034c02a63f080edc54b37dd5b03e0ea2d
スコッチ <43601554+swallow-ds@users.noreply.github.com>
2023-05-18 07:50:26 +0900

    Information content change

--- static/record/js/vgp.js
+++ static/record/js/vgp.js
@@ -29,34 +29,34 @@ $('ul#dropdown-language li').click((e) => {
 
 let infoTranslations = {
     en: [
-        { '2Usable in a local environment': 'CaseSharing can be used in a local environment, allowing doctors and nurses to save case information on their own computers. This contributes to smoother information sharing and access within the hospital, and helps to streamline medical practices.' },
-        { 'Try with sample data': 'Try out the system\'s functionality with sample data to get a better understanding of its operation and the components of case information.' },
-        { 'Encouraging collaboration': 'In the future, we plan to expand the system\'s capabilities to enable smoother information sharing with other systems. This will lead to increased efficiency and productivity through better collaboration.' },
-        { 'Multilingual support': 'The system supports languages such as English, Japanese, Korean, and Chinese, allowing people around the world to use the system in their own language and facilitating communication with users from different regions.' }
+        { 'What is Panel Search?': 'Panel Search is a publicly-available database that allows you to search and utilize Virtual Gene Panels associated with 9,998 human diseases. Virtual Gene Panels can be used for efficient clinical interpretation of genome sequences.' },
+        { 'How to construct Virtual Gene Panels': 'Based on the Monarch Disease Ontology (Mondo), which includes many rare and genetic diseases, we have constructed Virtual Gene Panels that automatically summarize disease-causing genes for each disease. By using the disease hierarchy information in Mondo, we have also automatically constructed Virtual Gene Panels for disease groups such as Mendelian disease for example.' },
+        { 'Data sources': 'Virtual Gene Panels was constructed from the following data source</br>・Mondo: <a href="http://purl.obolibrary.org/obo/mondo.obo">http://purl.obolibrary.org/obo/mondo.obo</a></br>・MedGen: <a href="https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen" target="_blank">https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen</a></br>・Orphanet: <a href="http://www.orphadata.org/data/xml/en_product6.xml" target="_blank">http://www.orphadata.org/data/xml/en_product6.xml</a></br>・GenCC: <a href="https://search.thegencc.org/download" target="_blank">https://search.thegencc.org/download</a>' },
+        { 'Disclaimer': 'Please refer to <a href="https://pubcasefinder.dbcls.jp/termsofservice?lang=en" target="_blank">this link</a>.' }
     ],
     ja: [
-        { 'ローカル環境で使える': 'CaseSharingはローカル環境で使え、医師や看護師などが症例情報を自分のコンピューターに保存できることを意味します。これにより、病院内の情報共有やアクセスがスムーズになり、医療現場の効率化に貢献します。' },
-        { 'サンプルデータで試す': 'サンプルデータを用いて本システムの使用イメージをつかみましょう。本システムの操作方法や症例情報の構成要素について理解を深めることができます。' },
-        { 'コラボレーションを促進': '将来、他のシステムとスムーズに情報の共有ができるように、機能を拡張する予定です。情報の共有により業務の効率化や生産性向上につながります。' },
-        { '多言語化': '本システムは英語、日本語、韓国語、中国語などの言語に対応しています。これにより、世界中の人々が、自分の言語で本システムを利用できるようになり、世界各地のユーザーとのコミュニケーションもスムーズになります。' }
+        { 'Panel Searchとは？': 'Panel Searchは、ヒトの9,998疾患に関連するVirtual Gene Panelを検索し、利活用できる公開データベースです。Virtual Gene Panelはゲノム配列の効率的な臨床的解釈に役立てることができます。' },
+        { 'Virtual Gene Panel構築方法': '多くの希少・遺伝性疾患を含むMonarch Disease Ontology（Mondo）をベースに、それぞれの疾患について疾患原因遺伝子を自動でまとめたVirtual Gene Panelを構築しました。Mondoの疾患階層情報を利用することで、例えば「Mendelian disease」などの疾患グループのVirtual Gene Panelも自動で構築しています。' },
+        { 'データソース': '以下のデータソースからVirtual Gene Panelを構築しました。</br>・Mondo: <a href="http://purl.obolibrary.org/obo/mondo.obo">http://purl.obolibrary.org/obo/mondo.obo</a></br>・MedGen: <a href="https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen" target="_blank">https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen</a></br>・Orphanet: <a href="http://www.orphadata.org/data/xml/en_product6.xml" target="_blank">http://www.orphadata.org/data/xml/en_product6.xml</a></br>・GenCC: <a href="https://search.thegencc.org/download" target="_blank">https://search.thegencc.org/download</a>' },
+        { '免責事項': '<a href="https://pubcasefinder.dbcls.jp/termsofservice" target="_blank">リンク先</a>を参照してください。' }
     ],
     ko: [
-        { '로컬 환경에서 사용 가능합니다.': 'CaseSharing은 로컬 환경에서 사용할 수 있으며, 의사나 간호사 등이 증례 정보를 자신의 컴퓨터에 저장할 수 있습니다. 이로 인해 병원 내 정보 공유와 접근이 원활해지며, 의료 현장의 효율화에 기여합니다.' },
-        { '샘플 데이터로 시도해 보세요.': '샘플 데이터를 이용하여 본 시스템의 사용 이미지를 파악할 수 있습니다. 본 시스템의 조작 방법이나 증례 정보의 구성 요소에 대해 이해를 깊이 드실 수 있습니다.' },
-        { '협업을 촉진합니다.': '앞으로 다른 시스템과 원활하게 정보 공유가 가능하도록 기능을 확장할 예정입니다. 정보 공유를 통해 업무의 효율화와 생산성 향상에 이어집니다.' },
-        { '다국어화': '본 시스템은 영어, 일본어, 한국어, 중국어 등의 언어를 지원합니다. 이를 통해 전 세계 사람들이 자신의 언어로 본 시스템을 이용할 수 있게 되어, 세계 각지의 사용자들과의 커뮤니케이션도 원활해집니다.' }
+        { 'Panel Search란?': 'Panel Search는 인간의 9,998 질환과 관련된 Virtual Gene Panel을 검색하고 활용할 수 있는 공개 데이터베이스 입니다.Virtual Gene Panel은 게놈 배열의 효율적인 임상적 해석에 도움이 될 수 있습니다.' },
+        { 'Virtual Gene Panel 구축 방법': '많은 희귀・유전성 질환명을 포함하는 Monarch Disease Ontology(Mondo)를 베이스로, 각각의 질환에 대해 질환 원인 유전자를 자동으로 정리한 Virtual Gene Panel을 구축했습니다.Mondo의 질환 계층 정보를 이용함으로써 예를 들어 「Mendelian disease」 등의 질환 그룹인 Virtual Gene Panel도 자동으로 구축하고 있습니다.' },
+        { '데이터 소스': '이하의 데이터 소스로부터 Virtual Gene Panel을 구축했습니다.</br>・Mondo: <a href="http://purl.obolibrary.org/obo/mondo.obo">http://purl.obolibrary.org/obo/mondo.obo</a></br>・MedGen: <a href="https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen" target="_blank">https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen</a></br>・Orphanet: <a href="http://www.orphadata.org/data/xml/en_product6.xml" target="_blank">http://www.orphadata.org/data/xml/en_product6.xml</a></br>・GenCC: <a href="https://search.thegencc.org/download" target="_blank">https://search.thegencc.org/download</a>' },
+        { '면책 사항': '<a href="https://pubcasefinder.dbcls.jp/termsofservice?lang=ko" target="_blank">링크</a>를 참조해 주시기 바랍니다.' }
     ],
     zh: [
-        { '本地环境可用': 'CaseSharing可在本地环境中使用，意味着医生、护士等可以将病例信息保存在自己的计算机上。这将使医院内的信息共享和访问更加顺畅，为医疗现场的效率提供贡献。' },
-        { '使用示例数据进行尝试': '使用示例数据来了解本系统的使用情况。您可以深入了解本系统的操作方法和病例信息的组成要素。' },
-        { '促进协作': '未来，我们计划扩展功能，以便与其他系统顺畅共享信息。信息共享将促进业务效率和生产力的提高。' },
-        { '多语言化': '本系统支持英语、日语、韩语、中文等多种语言。这将使世界各地的人们可以使用自己的语言使用本系统，与全球用户的沟通也将更加顺畅。' }
+        { '什么是面板搜索？': '面板搜索是一个公开的数据库, 允许你搜索和利用与9,998种人类疾病相关的虚拟基因面板。虚拟基因小组可用于对基因组序列的有效临床解释。' },
+        { '如何构建虚拟基因板': '基于Monarch疾病本体论（Mondo）, 其中包括许多罕见和遗传性疾病, 我们已经构建了虚拟基因面板, 自动总结每种疾病的致病基因。通过使用Mondo中的疾病层次信息, 我们还为疾病组（例如 "孟德尔病"）自动构建了虚拟基因面板。' },
+        { '数据来源': '虚拟基因面板是由以下数据来源构建的。</br>・Mondo: <a href="http://purl.obolibrary.org/obo/mondo.obo">http://purl.obolibrary.org/obo/mondo.obo</a></br>・MedGen: <a href="https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen" target="_blank">https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen</a></br>・Orphanet: <a href="http://www.orphadata.org/data/xml/en_product6.xml" target="_blank">http://www.orphadata.org/data/xml/en_product6.xml</a></br>・GenCC: <a href="https://search.thegencc.org/download" target="_blank">https://search.thegencc.org/download</a>' },
+        { '免责声明': '请参考<a href="https://pubcasefinder.dbcls.jp/termsofservice?lang=en" target="_blank">这个链接</a>。' }
     ],
     zhct: [
-        { '本地環境可使用。': 'CaseSharing 可在本地環境中使用，醫生和護士等可以將病例信息保存在自己的計算機上。這樣一來，醫院內的信息共享和訪問將變得更加順暢，有助於提高醫療效率。CaseSharing 可在本地環境中使用，醫生和護士等可以將病例信息保存在自己的計算機上。這樣一來，醫院內的信息共享和訪問將變得更加順暢，有助於提高醫療效率。' },
-        { '使用樣例數據進行試用。': '利用樣例數據來瞭解本系統的使用方法。您可以深入理解本系統的操作方法和病例信息的組成要素。' },
-        { '促進協作。': '未來，我們計劃擴展功能，使其能夠與其他系統順暢共享信息。信息共享有助於提高工作效率和生產力。' },
-        { '多語言化。': '本系統支持英語、日語、韓語、中文等多種語言。這樣一來，全球各地的人們都可以使用自己的語言使用本系統，與世界各地的用戶進行順暢的溝通。' }
+        { '什麼是面板搜索？': 'Panel Search 是一個公開可用的數據庫，可讓您搜索和利用與 9,998 種人類疾病相關的虛擬基因面板。 Virtual Gene Panels 可用於對基因組序列進行高效的臨床解釋。' },
+        { '如何構建虛擬基因面板': '基於包括許多罕見和遺傳疾病的君主疾病本體論 (Mondo)，我們構建了虛擬基因面板，可以自動總結每種疾病的致病基因。 通過使用 Mondo 中的疾病等級信息，我們還自動構建了針對疾病組的虛擬基因面板，例如“孟德爾疾病”。' },
+        { '數據源': 'Virtual Gene Panels 是根據以下數據源構建的。</br>・Mondo: <a href="http://purl.obolibrary.org/obo/mondo.obo">http://purl.obolibrary.org/obo/mondo.obo</a></br>・MedGen: <a href="https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen" target="_blank">https://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen</a></br>・Orphanet: <a href="http://www.orphadata.org/data/xml/en_product6.xml" target="_blank">http://www.orphadata.org/data/xml/en_product6.xml</a></br>・GenCC: <a href="https://search.thegencc.org/download" target="_blank">https://search.thegencc.org/download</a>' },
+        { '免責聲明': '請參考<a href="https://pubcasefinder.dbcls.jp/termsofservice?lang=en" target="_blank">此鏈接</a>。' }
     ]
 }
 
@@ -65,7 +65,7 @@ modalContent.innerHTML = `
     <ul>
         <li>
             <i class="material-symbols-outlined">
-            dvr
+            search
             </i>
             <span>
                 <h4>${Object.keys(infoTranslations[lang][0])}</h4>
@@ -75,7 +75,7 @@ modalContent.innerHTML = `
         </li>
         <li>
         <i class="material-symbols-outlined">
-        demography
+        view_list
         </i>
             <span>
                 <h4>${Object.keys(infoTranslations[lang][1])}</h4>
@@ -85,7 +85,7 @@ modalContent.innerHTML = `
         </li>
         <li>
         <i class="material-symbols-outlined">
-            handshake
+            database
             </i>
             <span>
                 <h4>${Object.keys(infoTranslations[lang][2])}</h4>
@@ -95,7 +95,7 @@ modalContent.innerHTML = `
         </li>
         <li>
         <i class="material-symbols-outlined">
-        keyboard_previous_language
+        warning
         </i>
             <span>
                 <h4>${Object.keys(infoTranslations[lang][3])}</h4>
